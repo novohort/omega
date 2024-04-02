029bbb2d24e0f9f0b42327d25e8e1e8383f2630b
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-01 20:31:34 -0500

    repl boolean objects

--- interpreter/evaluator/evaluator.go
+++ interpreter/evaluator/evaluator.go
@@ -5,12 +5,19 @@ import (
 	"interpreter/object"
 )
 
+var (
+	TRUE	= &object.Boolean{Value: true}
+	FALSE	= &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
@@ -25,4 +32,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }

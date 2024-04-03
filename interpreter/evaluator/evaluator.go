d6ef39ed0640d9c77bfb888245a4b3b22ae96da4
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-02 20:01:57 -0500

    evaluating index operator expressions

--- interpreter/evaluator/evaluator.go
+++ interpreter/evaluator/evaluator.go
@@ -78,8 +78,48 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 	return nil
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	if left == nil || index == nil {
+		return newError("nil argument to index expression")
+	}
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject, ok := array.(*object.Array)
+	if !ok {
+		return newError("index left-hand side is not an array")
+	}
+
+	idxObject, ok := index.(*object.Integer)
+	if !ok {
+		return newError("index right-hand side not an interger")
+	}
+
+	idx := idxObject.Value
+	if idx < 0 || idx >= int64(len(arrayObject.Elements)) {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {

89d58245f21e787126705869097fe98fb5f7ea47
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-03 15:05:18 -0500

    expand macros

--- interpreter/evaluator/macro_expansion_test.go
+++ interpreter/evaluator/macro_expansion_test.go
@@ -8,6 +8,42 @@ import (
 	"testing"
 )
 
+func TestExpandMacros(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	}{
+	{
+		`
+		let infixExpression { macro() { quote(1 + 2); }};
+		infixExpression();
+		`,
+		`(1 + 2)`,
+	},
+	{
+		`
+		let reverse { macro(a, b) { quote(unquote(b) - unquote(a)); }};
+		reverse(2 + 2, 10 - 5);
+		`,
+		`(10 - 5) - (2 + 2)`,
+	},
+	}
+
+	for _, tt := range tests {
+		expected := testParseProgram(tt.expected)
+		program := testParseProgram(tt.input)
+
+		env := object.NewEnvironment()
+		DefineMacros(program, env)
+		expanded := ExpandMacros(program, env)
+
+		if expanded.String() != expected.String() {
+			t.Errorf("not equal. want=%q, got=%q",
+				expected.String(), expanded.String())
+		}
+	}
+}
+
 func TestDefineMacros(t *testing.T) {
 	input := `
 	let number { 1 };

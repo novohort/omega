1b5408388c1566e69521e9f6b2618b5b84730b22
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-02 20:07:09 -0500

    add builtin: first

--- interpreter/evaluator/builtins.go
+++ interpreter/evaluator/builtins.go
@@ -3,6 +3,25 @@ package evaluator
 import "interpreter/object"
 
 var builtins = map[string]*object.Builtin {
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+						len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must by ARRAY, got %s",
+						args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		}
+	}
 	"len": &object.Builtin {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {

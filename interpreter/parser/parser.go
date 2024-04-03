946bdf6b2cba873973537d4bc19dd44cd4b9bd2c
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-02 19:20:00 -0500

    parse array literals

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -93,7 +93,40 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
+	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
 	return p
+}
+
+func (p *Parser) parseArrayLiteral() ast.Expression {
+	array := &ast.ArrayLiteral{Token: p.curToken}
+
+	array.Elements = p.parseExpressionList(token.RBRACKET)
+
+	return array
+}
+
+func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
+	list := []ast.Expression{}
+
+	if p.peekTokenIs(end) {
+		p.nextToken()
+		return list
+	}
+
+	p.nextToken()
+	list = append(list, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		list = append(list, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(end) {
+		return nil
+	}
+
+	return list
 }
 
 func (p *Parser) parseStringLiteral() ast.Expression {
@@ -102,7 +135,7 @@ func (p *Parser) parseStringLiteral() ast.Expression {
 
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	exp := &ast.CallExpression{Token: p.curToken, Function: function}
-	exp.Arguments = p.parseCallArguments()
+	exp.Arguments = p.parseExpressionList(token.RPAREN)
 	return exp
 }
 
--- interpreter/token/token.go
+++ interpreter/token/token.go
@@ -58,6 +58,8 @@ const (
 	LBRACE		= "{"
 	RBRACE		= "}"
 	COLON			= ":"
+	LBRACKET	= "["
+	RBRACKET	=	"]"
 
 	// keywords
 	FUNCTION	= "FUNCTION"

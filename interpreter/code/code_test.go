7101418a23df7de9ecd78b46c186550fc6e70435
Zebulun McNeill <zeb.mcn@gmail.com>
2024-04-09 12:48:10 -0500

    flesh out string func for code test

--- interpreter/code/code_test.go
+++ interpreter/code/code_test.go
@@ -40,8 +40,9 @@ func TestInstructionsString(t *testing.T) {
 	}
 
 	expected := `0000 OpConstant 1
-	0003 OpConstant 2
-	0006 OpConstant 65535`
+0003 OpConstant 2
+0006 OpConstant 65535
+`
 
 	concatted := Instructions{}
 	for _, ins := range instructions {
